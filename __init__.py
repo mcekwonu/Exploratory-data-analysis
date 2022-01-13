4dbd29b1b19362c7816dde112730e383391706ca
mcekwonu <cekwonu@gmail.com>
2022-01-14 05:13:11 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from . import data_analysis
+
+
+__author__ = "Michael Chukwuemeka Ekwonu"
+
+__email__ = "deepstarai@gmail.com"
+
+__version__ = "0.1.0"
+
+__date__ = "2022.01.09"
+
+__url__ = "https:github.com/mcekwonu"
+
+
+__all__ = ['data_analysis']
+
